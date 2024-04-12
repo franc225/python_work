dbaa1b6a374068b304dfd7c33e8b9ae1af4fd6b2
François <francois.beauregard.225@gmail.com>
2024-04-12 19:15:15 -0400

    Python Crash Course Chapter 9

---
+++ chapter9-1.py
@@ -0,0 +1,12 @@
+import dog
+
+my_dog = dog.Dog('Willie',6)
+my_dog.sit()
+my_dog.roll_over()
+
+from random import randint
+print(randint(1,6))
+
+from random import choice
+players = ['charles','martina','michael','florence','eli']
+print(choice(players))
