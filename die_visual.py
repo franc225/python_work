3c5dc65f5cbac02668b64c1e2245f8de96bf1747
François <francois.beauregard.225@gmail.com>
2024-05-05 11:02:48 -0400

    Python Crash Course Chapter 15

---
+++ die_visual.py
@@ -0,0 +1,24 @@
+import plotly.express as px
+
+from die import Die
+
+die = Die()
+
+results = []
+
+for roll_num in range(100):
+    result = die.roll()
+    results.append(result)
+
+print(results)
+
+frequencies = []
+poss_results = range(1,die.num_sides + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
+
+fig = px.bar(x=poss_results, y=frequencies)
+fig.show()
---
+++ rw_visual.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+rw = RandomWalk()
+rw.fill_walk()
+plt.style.use('classic')
+fig, ax = plt.subplots()
+point_numbers=range(rw.num_points)
+ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+ax.set_aspect('equal')
+plt.show()
